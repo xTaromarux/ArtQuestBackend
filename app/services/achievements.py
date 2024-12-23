0d8c38906617765643d1d9d0c3e5bb766fdd17e8
MałgorzataTomiło <gosiatomilo@gmail.com>
2024-12-23 15:34:56 +0100

    Remove some not used files

--- app/services/achievements.py
+++ app/services/achievements.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from database import get_db
-from achievements import Achievements
+from models import Achievements
 
 router = APIRouter()
 
