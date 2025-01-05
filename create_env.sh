dd0f1e2ca72611230fc29a8a616716050a40804c
MałgorzataTomiło <gosiatomilo@gmail.com>
2025-01-05 11:30:33 +0100

    modify docker file

--- create_env.sh
+++ create_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python3 -m virtualenv --no-download -p python3.8.10 venv
+pip3 install virtualenv --user
+python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r app/requirements.txt
 pip3 install -r app/ai_model/requirements.txt
